34f392aff1b18c1630e9a13efe962a0a2750cfcd
Vinicius Cavalcanti <vincius.cavalcanti16@gmail.com>
2019-02-08 17:46:46 -0200

    adicionado: crud para ProductType

--- src/main/java/com/test/api/product/application/service/ProductTypeService.java
+++ src/main/java/com/test/api/product/application/service/ProductTypeService.java
@@ -4,9 +4,10 @@ import com.test.api.product.application.dto.ProductTypeDTO;
 import com.test.api.product.domain.assemble.ProductTypeAssemble;
 import com.test.api.product.domain.entity.ProductType;
 import com.test.api.product.domain.repository.ProductTypeRepository;
-import com.test.api.product.domain.resouce.ProductTypeResource;
+import com.test.api.product.domain.resource.ProductTypeResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
@@ -20,24 +21,43 @@ public class ProductTypeService {
         this.productTypeRepository = productTypeRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<ProductTypeResource> getAll() {
         var productsTypes = productTypeRepository.findAll();
         var assemble = new ProductTypeAssemble();
         return assemble.toResources(productsTypes);
     }
 
+    @Transactional(readOnly = true)
     public ProductTypeResource getById(Integer id) {
-        var productType = productTypeRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("ProductType by id: " + id + " Not found"));
+        var productType = getProductType(id);
         var assemble = new ProductTypeAssemble();
         return assemble.toResource(productType);
     }
 
+    @Transactional
     public ProductTypeResource save(ProductTypeDTO productTypeDTO) {
-        return null;
+        var productType = new ProductType();
+        replaceEntity(productTypeDTO, productType);
+        var assemble = new ProductTypeAssemble();
+        return assemble.toResource(productTypeRepository.save(productType));
     }
 
+    @Transactional
     public ProductTypeResource update(Integer id, ProductTypeDTO productTypeDTO) {
-        return null;
+        var productType = getProductType(id);
+        replaceEntity(productTypeDTO, productType);
+        var assemble = new ProductTypeAssemble();
+        return assemble.toResource(productType);
+    }
+
+    private void replaceEntity(ProductTypeDTO dto, ProductType productType) {
+        productType.setName(dto.getName());
+        productType.setTimeProcess(dto.getTimeProcess());
+    }
+
+    private ProductType getProductType(Integer id) {
+        return productTypeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("ProductType by id: " + id + " Not found"));
     }
 }
--- src/main/java/com/test/api/product/domain/entity/ProductType.java
+++ src/main/java/com/test/api/product/domain/entity/ProductType.java
@@ -3,18 +3,19 @@ package com.test.api.product.domain.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "product_type")
 @Data
-public class ProductType {
+public class ProductType implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, length = 50)
+    @Column(nullable = false, length = 50, unique = true)
     private String name;
 
     @Column(nullable = false)
